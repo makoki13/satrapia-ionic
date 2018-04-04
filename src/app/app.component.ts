25e7e83382543d09dc27dc81692f36e98fbfca9e
makoki <pablo.makoki@gmail.com>
2018-04-04 15:35:21 +0200

    mejora inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,6 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private storage: Storage) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-
       //this.storage.remove('usuario3');this.storage.remove('usuario4');this.storage.remove('usuario5');
       //this.storage.remove('inicio');this.storage.remove('num-usuarios');this.storage.remove('sesion-usuario');
 
@@ -27,35 +24,26 @@ export class MyApp {
         console.log('sesion-usuario',val);
         if ( (!val) || (val.trim() === '') )  {
           console.log('NO hay sesion-usuario');
+          this.storage.get('num-usuarios').then((val) => {
+            console.log('num-usuarios:',val);
+
+            if ( (!val) || (val === 0) )  {
+              this.rootPage = RegistroPage;
+            } else {
+              this.rootPage = LoginPage;
+            }
+          })
+          .catch( (val) => {
+            console.log('catch',val);
+            this.rootPage = RegistroPage; // Quitar despues de pruebas
+          })  ;
         }
         else {
           console.log('SI hay sesion-usuario');
           this.rootPage = HomePage;
         }
+
       });
-
-      this.storage.get('inicio').then((val) => {
-        console.log('inicio:',val);
-
-        if (val === 'home') {
-          this.rootPage = HomePage;
-        }
-        else if (val === 'login') {
-          this.rootPage = LoginPage;
-        }
-        else if (val === '') {
-          this.rootPage = RegistroPage;
-        }
-        else {
-          this.rootPage = RegistroPage;
-        }
-
-        //this.rootPage = HomePage; // Quitar despues de pruebas
-      })
-      .catch( (val) => {
-        console.log('catch',val);
-        this.rootPage = RegistroPage; // Quitar despues de pruebas
-      })  ;
 
       statusBar.styleDefault();
       splashScreen.hide();
